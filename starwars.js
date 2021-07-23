f097fb21b75417a8f008c58bcdc0fc04b55eb12e
Antônio Sousa <antonioaads@gmail.com>
2021-07-22 22:53:39 -0300

    feat: ex 1 - Done

--- starwars.js
+++ starwars.js
@@ -5,3 +5,18 @@
 //  - Quando um filme for clicado, exibir sua introdução
 
 const API_ENDPOINT = 'https://swapi.dev/api'
+
+// Exercício 1
+import { AudioPlayer } from "./music.js"
+//import { AudioPlayer } from "./music-sem-private.js"
+
+const audioPlayer = new AudioPlayer()
+audioPlayer.start(
+  { 
+    audioUrl: 'audio/tema-sw.mp3', 
+    coverImageUrl:'imgs/logo.svg', 
+    title: 'Intro', 
+    artist: 'John Williams' 
+  }, 
+  document.body
+)
